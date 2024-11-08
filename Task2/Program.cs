c5a44209993136313952db44f6aa2a485ec7aa6f
Emmanuel Michael Ikechukwu <162467554+Emmanuel-Michael05@users.noreply.github.com>
2024-11-08 14:02:09 +0100

    Add project files.

---
+++ Task2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //user input
+            Console.WriteLine("Hello, enter a number of choice   ");
+            
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            if (userInput % 2 == 0)
+            {
+                Console.WriteLine("Your number is even.");
+            }
+            else
+            {
+                Console.WriteLine("Your number is odd.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
